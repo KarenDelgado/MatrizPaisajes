f0c47165f1e07c6491a39a66b812fa24e3732854
KarenDelgado <48525229+KarenDelgado@users.noreply.github.com>
2024-03-06 13:54:49 -0600

    Funcionalidad con js

---
+++ js/javascript.js
@@ -0,0 +1,257 @@
+function displayDate() {
+  document.getElementById("fechaFooter").innerHTML = Date();
+}
+
+function borrar() {
+  document.getElementById("resultado").innerHTML="";
+}
+
+function showContent1() {
+    element = document.getElementById("content1");
+    check = document.getElementById("check1");
+    if (check.checked) {
+      element.style.display='block';
+    }
+    else {
+      element.style.display='none';
+    }
+  }
+  function showContent2() {
+    element = document.getElementById("content2");
+    check = document.getElementById("check2");
+    if (check.checked) {
+      element.style.display='block';
+    }
+    else {
+      element.style.display='none';
+    }
+  }
+  function showContent3() {
+    element = document.getElementById("content3");
+    check = document.getElementById("check3");
+    if (check.checked) {
+      element.style.display='block';
+    }
+    else {
+      element.style.display='none';
+    }
+  }
+  function showContent4() {
+    element = document.getElementById("content4");
+    check = document.getElementById("check4");
+    if (check.checked) {
+      element.style.display='block';
+    }
+    else {
+      element.style.display='none';
+    }
+  }
+  function showContent5() {
+    element = document.getElementById("content5");
+    check = document.getElementById("check5");
+    if (check.checked) {
+      element.style.display='block';
+    }
+    else {
+      element.style.display='none';
+    }
+  }
+  function showContent6() {
+    element = document.getElementById("content6");
+    check = document.getElementById("check6");
+    if (check.checked) {
+      element.style.display='block';
+    }
+    else {
+      element.style.display='none';
+    }
+  }
+   function showContent7() {
+    element = document.getElementById("content7");
+    check = document.getElementById("check7");
+    if (check.checked) {
+      element.style.display='block';
+    }
+    else {
+      element.style.display='none';
+    }
+  }
+   function showContent8() {
+    element = document.getElementById("content8");
+    check = document.getElementById("check8");
+    if (check.checked) {
+      element.style.display='block';
+    }
+    else {
+      element.style.display='none';
+    }
+  }
+   function showContent9() {
+    element = document.getElementById("content9");
+    check = document.getElementById("check9");
+    if (check.checked) {
+      element.style.display='block';
+    }
+    else {
+      element.style.display='none';
+    }
+  }
+   function showContent10() {
+    element = document.getElementById("content10");
+    check = document.getElementById("check10");
+    if (check.checked) {
+      element.style.display='block';
+    }
+    else {
+      element.style.display='none';
+    }
+  }
+   function showContent11() {
+    element = document.getElementById("content11");
+    check = document.getElementById("check11");
+    if (check.checked) {
+      element.style.display='block';
+    }
+    else {
+      element.style.display='none';
+    }
+  }
+   function showContent12() {
+    element = document.getElementById("content12");
+    check = document.getElementById("check12");
+    if (check.checked) {
+      element.style.display='block';
+    }
+    else {
+      element.style.display='none';
+    }
+  }
+   function showContent13() {
+    element = document.getElementById("content13");
+    check = document.getElementById("check13");
+    if (check.checked) {
+      element.style.display='block';
+    }
+    else {
+      element.style.display='none';
+    }
+  }
+   function showContent14() {
+    element = document.getElementById("content14");
+    check = document.getElementById("check14");
+    if (check.checked) {
+      element.style.display='block';
+    }
+    else {
+      element.style.display='none';
+    }
+  }
+   function showContent15() {
+    element = document.getElementById("content15");
+    check = document.getElementById("check15");
+    if (check.checked) {
+      element.style.display='block';
+    }
+    else {
+      element.style.display='none';
+    }
+  }
+   function showContent16() {
+    element = document.getElementById("content16");
+    check = document.getElementById("check16");
+    if (check.checked) {
+      element.style.display='block';
+    }
+    else {
+      element.style.display='none';
+    }
+  }
+   function showContent17() {
+    element = document.getElementById("content17");
+    check = document.getElementById("check17");
+    if (check.checked) {
+      element.style.display='block';
+    }
+    else {
+      element.style.display='none';
+    }
+  }
+   function showContent18() {
+    element = document.getElementById("content18");
+    check = document.getElementById("check18");
+    if (check.checked) {
+      element.style.display='block';
+    }
+    else {
+      element.style.display='none';
+    }
+  }
+   function showContent19() {
+    element = document.getElementById("content19");
+    check = document.getElementById("check19");
+    if (check.checked) {
+      element.style.display='block';
+    }
+    else {
+      element.style.display='none';
+    }
+  }
+   function showContent20() {
+    element = document.getElementById("content20");
+    check = document.getElementById("check20");
+    if (check.checked) {
+      element.style.display='block';
+    }
+    else {
+      element.style.display='none';
+    }
+  }
+
+/*function myFunction(id) {
+	var imagen = ["img/Foto_1.jpg","img/Foto_2.jpg","img/Foto 3.jpeg",
+	"img/Foto 4.jpeg","img/Foto 5.jpeg","img/Foto 6.jpeg","img/Foto 7.jpeg",
+	"img/Foto 8.jpeg","img/Foto 9.jpeg","img/Foto 10.jpeg","img/Foto 11.jpeg",
+	"img/Foto 12.jpeg","img/Foto 13.jpeg","img/Foto 14.jpeg","img/Foto 15.jpeg",
+	"img/Foto 16.jpg","img/Foto 17.jpg","img/Foto 18.png","img/Foto 19.jpg",
+	"img/Foto 20.jpg"];
+	
+  var checkBox = document.getElementById("myCheck");
+  var foto = document.getElementById(id);
+  if (checkBox.checked == true){
+  	foto.src = imagen[id];
+    foto.style.display = "block";
+  } else {
+    foto.style.display = "none";
+  }
+}
+
+	var elegir = document.getElementById(id)
+	if(elegir.style.visibility=="hidden"){
+		document.getElementById(id).src = none;
+		elegir.style.visibility=="visible";
+	}
+	else{
+		elegir.style.visibility=="hidden";
+		document.getElementById(id).src = imagen[id];
+	}
+
+
+	 var checkBox = document.getElementById("myCheck");
+  var foto = document.getElementById(id);
+  switch (id) {
+  case '0':
+   	if (checkBox.checked == true){
+  		foto.src = imagen[0];
+    	foto.style.display = "block";
+ 	} else 
+    	foto.style.display = "none";
+    break;
+  case '1':
+    if (checkBox.checked == true){
+  		foto.src = imagen[1];
+    	foto.style.display = "block";
+ 	} else 
+    	foto.style.display = "none";
+    break;
+  }
+}*/
